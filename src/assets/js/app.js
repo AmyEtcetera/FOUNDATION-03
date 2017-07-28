e6106a35b159c397c5e4e20440f9a6c558961e55
Amy L Letson <asemenuk@temple.edu>
2017-07-28 16:22:18 -0400

    Initial commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-
 window.$ = $;
 
 import Foundation from 'foundation-sites';
@@ -8,6 +7,31 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
 $(document).foundation();
 
+
+    // my stuff
+// console.log('say why');
+
+  // okay you can put the jquery right here, nbd
+ //$("#powerbutton").click(function(){
+//     $("p").toggle();
+ //});
+
+
+    // move this somewhere else if it works
+// $.getJSON('data.json', function(info){
+//     var output='';
+//     for (var i = 0; i <= info.links.length-1; i++) {
+//       for (key in info.links[i]) {
+//         if (info.links[i].hasOwnProperty(key)) {
+//           output += '<li>' +
+//           '<a href = "' + info.links[i][key] +
+//           '">' + key + '</a>';
+//           '</li>';
+//         }
+//       }
+//     }
+//     var update = document.getElementById('links');
+//     update.innerHTML = output;
+// }); //getJSON
